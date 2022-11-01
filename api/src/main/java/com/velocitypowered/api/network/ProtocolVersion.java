e06eb3ddff3bf78630b68c41318d9b6bf3839789
Quaint <thesiberiantigercat@gmail.com>
2022-11-01 03:22:44 -0400

    22w43a

--- api/src/main/java/com/velocitypowered/api/network/ProtocolVersion.java
+++ api/src/main/java/com/velocitypowered/api/network/ProtocolVersion.java
@@ -60,7 +60,7 @@ public enum ProtocolVersion {
   MINECRAFT_1_18_2(758, "1.18.2"),
   MINECRAFT_1_19(759, "1.19"),
   MINECRAFT_1_19_1(760, "1.19.1", "1.19.2"),
-  MINECRAFT_1_20(-1, 102, "1.20"); // 22w42a
+  MINECRAFT_1_20(-1, 103, "1.20"); // 22w43a
 
   private static final int SNAPSHOT_BIT = 30;
 
